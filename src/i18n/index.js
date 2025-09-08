41b98942980de0403adda05db6a231f6ffb5e817
KhongCoTen-code <luutrulaptop4@gmail.com>
2025-09-08 22:21:32 +0700

    Việt hóa giao diện JS

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -2,19 +2,22 @@ import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 // ivew UI
 import ivenUS from 'view-design/dist/locale/en-US'
-import ivzhCN from 'view-design/dist/locale/zh-CN'
-import ivzhTW from 'view-design/dist/locale/zh-TW'
+import ivenVN from 'view-design/dist/locale/vi-VN'
+// import ivzhCN from 'iview/dist/locale/zh-CN'
+// import ivzhTW from 'iview/dist/locale/zh-TW'
 // element UI
 import elenUS from 'element-ui/lib/locale/lang/en'
-import elzhCN from 'element-ui/lib/locale/lang/zh-CN'
-import elzhTW from 'element-ui/lib/locale/lang/zh-TW'
+import elenVN from 'element-ui/lib/locale/lang/vi'
+// import elzhCN from 'element-ui/lib/locale/lang/zh-CN'
+// import elzhTW from 'element-ui/lib/locale/lang/zh-TW'
 
 Vue.use(VueI18n)
 
 const languages = [
-  {value: 'en-US', label: 'English', iv: ivenUS, el: elenUS},
-  {value: 'zh-CN', label: '简体中文', iv: ivzhCN, el: elzhCN},
-  {value: 'zh-TW', label: '繁體中文', iv: ivzhTW, el: elzhTW}
+  { value: 'en-US', label: 'English', iv: ivenUS, el: elenUS },
+  { value: 'vi-VN', label: 'Tiếng Việt', iv: ivenVN, el: elenVN }
+  // {value: 'zh-CN', label: '简体中文', iv: ivzhCN, el: elzhCN},
+  // {value: 'zh-TW', label: '繁體中文', iv: ivzhTW, el: elzhTW}
 ]
 const messages = {}
 
@@ -24,12 +27,12 @@ for (let lang of languages) {
   let m = require(`./oj/${locale}`).m
   Object.assign(m, require(`./admin/${locale}`).m)
   let ui = Object.assign(lang.iv, lang.el)
-  messages[locale] = Object.assign({m: m}, ui)
+  messages[locale] = Object.assign({ m: m }, ui)
 }
 // load language packages
 export default new VueI18n({
-  locale: 'zh-CN',
+  locale: 'vi-VN',
   messages: messages
 })
 
-export {languages}
+export { languages }
